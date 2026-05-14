525c7fabac0d81e5e3e2f6269bfb7ad3ccdfe967
parjdettg <dbag7351@gmail.com>
2026-05-14 20:46:06 +0900

    Key값 수정

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,14 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "네_메모장_값",
-  authDomain: "네_메모장_값",
-  projectId: "네_메모장_값",
-  storageBucket: "네_메모장_값",
-  messagingSenderId: "네_메모장_값",
-  appId: "네_메모장_값"
+  apiKey: "AIzaSyCjrqWteu1rLod6OU-6t-GxAY7WGcMWPXo",
+  authDomain: "jinju-coop-web.firebaseapp.com",
+  projectId: "jinju-coop-web",
+  storageBucket: "jinju-coop-web.firebasestorage.app",
+  messagingSenderId: "759661565652",
+  appId: "1:759661565652:web:cb36258d63fee2bee08d41"
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
